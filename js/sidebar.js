5fb36a694172dbf8fee4112beb5c0fd479040d17
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-01-14 19:58:01 +0100

    Edit atrribute example color

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,13 @@
 const menuIcon = document.querySelector(".menuIcon");
 const navbar = document.getElementById("navbar");
 const mainDoc = document.getElementById("main-doc");
+
 export const navLinks = document.querySelectorAll(".nav-link");
 
 function navbarToggle() {
   navbar.classList.toggle("hide");
   mainDoc.classList.toggle("adjust");
+  
 }
 menuIcon.addEventListener("click", () => {
   navbarToggle();
